c99e3467e4352bc2affe2575c37ef2fcde0bd4d4
Victor Johan <Vicjohan23@gmail.com>
2020-06-30 19:44:07 -0400

    SE CREO LA CLASE ProductosBLL

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPedidosSuplidor.DAL;
+using RegistroPedidosSuplidor.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegistroPedidosSuplidor.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos producto)
+        {
+            if (Existe(producto.ProductoId))
+                return Insertar(producto);
+            else
+                return Modificar(producto);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                ok = contexto.Productos.Any(o => o.ProductoId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+
+        private static bool Insertar(Productos producto)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Productos.Add(producto);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+
+        private static bool Modificar(Productos producto)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Entry(producto).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+            return ok;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos producto;
+            try
+            {
+                producto = contexto.Productos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return producto;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var encontrado = contexto.Productos.Find(id);
+                if (encontrado != null)
+                {
+                    contexto.Productos.Remove(encontrado);
+                    ok = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+
+        public static List<Productos> GetList()
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Productos.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
