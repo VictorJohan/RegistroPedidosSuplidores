7d606146650ac8a4d9545169b99a6ad5fbe6d895
Victor Johan <Vicjohan23@gmail.com>
2020-07-01 17:57:58 -0400

    SE CREO EL WPF rOrdenes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegistroPedidosSuplidor.UI.Registro;
 
 namespace RegistroPedidosSuplidor
 {
@@ -23,6 +24,12 @@ namespace RegistroPedidosSuplidor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            rOrdenes rOrdenes = new rOrdenes();
+            rOrdenes.Show();
         }
     }
 }
---
+++ UI/Registro/rOrdenes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using RegistroPedidosSuplidor.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegistroPedidosSuplidor.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for rOrdenes.xaml
+    /// </summary>
+    public partial class rOrdenes : Window
+    {
+        private Ordenes Orden = new Ordenes();
+        public rOrdenes()
+        {
+            InitializeComponent();
+            this.DataContext = Orden;
+        }
+    }
+}
